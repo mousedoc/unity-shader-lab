5eeb4250394a85256a59dd7605d6918f7cede1a5
mousedoc <sangyun3631@gmail.com>
2020-07-14 18:33:29 +0900

    Add Position,Rotation Variator

---
+++ Assets/Script/PositionVariator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionVariator : MonoBehaviour
+{
+    [SerializeField]
+    private bool byLocal = true;
+    [SerializeField]
+    private float speed = 3f;
+    [SerializeField]
+    private float offset = 0.5f;
+
+    [SerializeField]
+    private bool x = true;
+    [SerializeField]
+    private bool y = true;
+    [SerializeField]
+    private bool z = true;
+
+    private void Update()
+    {
+        var time = Time.time;
+        var position = byLocal ? transform.localPosition : transform.position;
+
+        if(x)
+            position.x = Mathf.Sin(time * speed) * offset;
+        if(y)
+            position.y = Mathf.Cos(time * speed) * offset;
+        if(z)
+            position.z = Mathf.Cos(time * speed) * offset;
+
+        if(byLocal)
+            transform.localPosition = position;
+        else
+            transform.position = position;
+    }
+}
---
+++ Assets/Script/RotationVariator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationVariator : MonoBehaviour
+{
+    [SerializeField]
+    private bool byLocal = true;
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private float offset = 0.5f;
+
+    [SerializeField]
+    private bool x = true;
+    [SerializeField]
+    private bool y = true;
+    [SerializeField]
+    private bool z = true;
+
+    private void Update()
+    {
+        var time = Time.time;
+        var rotation = byLocal ? transform.localRotation : transform.rotation;
+
+        if(x)
+            rotation.x = Mathf.Sin(time * speed) * offset;
+        if(y)
+            rotation.y = Mathf.Cos(time * speed) * offset;
+        if(z)
+            rotation.z = Mathf.Cos(time * speed) * offset;
+
+        if(byLocal)
+            transform.localRotation = rotation;
+        else
+            transform.rotation = rotation;
+    }
+}
