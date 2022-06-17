320172ab7db096a266c5911e304ab042937e5f0a
mousedoc <sangyun3631@gmail.com>
2022-06-17 17:12:59 +0900

    asdf

---
+++ Assets/Script/DepthCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthCamera : MonoBehaviour
+{
+    public Camera targetCamera;
+    public RenderTexture renderTexture;
+
+    void Start()
+    {
+        //renderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+        renderTexture.width = Screen.width;
+        renderTexture.height = Screen.height;
+        renderTexture.depth = 24;
+        renderTexture.format = RenderTextureFormat.Depth;
+
+
+        targetCamera.depthTextureMode = DepthTextureMode.Depth;
+        targetCamera.targetTexture = renderTexture;
+    }
+}
